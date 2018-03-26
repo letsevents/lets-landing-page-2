a35c213ca12a00977b860043dbf48915dc001a32
Samuel Brandão <gb.samuel@gmail.com>
2018-03-26 14:54:51 -0300

    Css adjustments requested by Lucas Campoi

--- templates-parts/form.php
+++ templates-parts/form.php
@@ -13,7 +13,7 @@
         <div class='from-bh'>
             <?php echo get_field('de') ?>
             <a href="https://www.google.it/maps/place/Belo+Horizonte,+MG/@-20.2506648,-65.1489347,3.5z/data=!4m5!3m4!1s0xa690cacacf2c33:0x5b35795e3ad23997!8m2!3d-19.9217127!4d-43.9343262" target="_blank">BH</a>
-            <?php echo get_field('to_the_world') ?>
+            <?php echo get_field('para_o_mundo') ?>
 
             <img data-src="<?php echo plugins_url('../img/Brasil-BH@2x.png', __FILE__) ?>" alt=""> <?php echo get_field('made_in_brazil') ?>
         </div>
